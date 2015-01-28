187c0c4ccccd366bea6fab4113d91207b45ff0fc
Stefan Bauer <sauberzof@gmail.com>
2015-01-28 12:09:26 +0100

    copyright year removed

--- lupo.php
+++ lupo.php
@@ -4,7 +4,7 @@
  *
  * @package		Joomla
  * @subpackage	LUPO
- * @copyright	Copyright (C) 2006 - 2014 databauer / Stefan Bauer
+ * @copyright	Copyright (C) databauer / Stefan Bauer
  * @author		Stefan Bauer
  * @link				http://www.ludothekprogramm.ch
  * @license		License GNU General Public License version 2 or later
