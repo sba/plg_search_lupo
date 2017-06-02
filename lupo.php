a9f338a99cdf744a8dc1856f6ecb7bbad774df59
Stefan Bauer <sauberzof@gmail.com>
2017-06-02 07:06:52 +0200

    added option to hide photo in search result

--- lupo.php
+++ lupo.php
@@ -4,7 +4,7 @@
  *
  * @package		Joomla
  * @subpackage	LUPO
- * @copyright   Copyright (C) databauer / Stefan Bauer 
+ * @copyright   Copyright (C) databauer / Stefan Bauer
  * @author		Stefan Bauer
  * @link		http://www.ludothekprogramm.ch
  * @license		License GNU General Public License version 2 or later
@@ -32,8 +32,8 @@ class plgSearchLupo extends JPlugin
 	}
 
 	/**
-	* @return array An array of search areas
-	*/
+	 * @return array An array of search areas
+	 */
 	function onContentSearchAreas()
 	{
 		static $areas = array(
@@ -43,13 +43,13 @@ class plgSearchLupo extends JPlugin
 	}
 
 	/**
-	* Search method
-	*
-	* The sql must return the following fields that are used in a common display
-	* routine: href, title, section, created, text, browsernav
-	* @param string Target search string
-	* @param string mathcing option, exact|any|all
-	* @param string ordering option, newest|oldest|popular|alpha|category
+	 * Search method
+	 *
+	 * The sql must return the following fields that are used in a common display
+	 * routine: href, title, section, created, text, browsernav
+	 * @param string Target search string
+	 * @param string mathcing option, exact|any|all
+	 * @param string ordering option, newest|oldest|popular|alpha|category
 	 */
 	function onContentSearch($text, $phrase='', $ordering='', $areas=null)
 	{
@@ -63,16 +63,17 @@ class plgSearchLupo extends JPlugin
 		$user	= JFactory::getUser();
 		$groups	= implode(',', $user->getAuthorisedViewLevels());
 
-        $request_type = JRequest::getVar('type');
+		$request_type = JRequest::getVar('type');
 
 		if (is_array($areas)) {
 			if (!array_intersect($areas, array_keys($this->onContentSearchAreas()))) {
 				return array();
 			}
 		}
-		
-		$limit			= $this->params->def('search_limit',		50);
-		$params_itemid         = $this->params->def('Itemid',		'');
+
+		$limit			= $this->params->def('search_limit',50);
+		$foto_show      = $this->params->def('foto_show','1');
+		$params_itemid  = $this->params->def('Itemid','');
 
 		$text = trim($text);
 		if ($text == '') {
@@ -130,39 +131,41 @@ class plgSearchLupo extends JPlugin
 		$query->where($where . ' OR a.number = '. $db->quote($db->escape($text, true)));
 		$query->order($order);
 		$db->setQuery($query, 0, $limit);
-		
+
 		$rows = $db->loadObjectList();
 
 		if ($rows) {
 			foreach($rows as $key => $row) {
 
-                //get Itemid for each search-result. Seach first for lupo-category menuitem, if not set take plugin-Itemid if set
-                $query	= $db->getQuery(true);
-                $query->select('m.id');
-                $query->from('#__menu AS m');
-                $query->where('m.link = "index.php?option=com_lupo&view=category&id='.$row->catslug.'"');
-                $db->setQuery($query);
-                $menu = $db->loadAssoc();
-                $itemid = "";
-                if(isset($menu['id'])){
-                    $itemid = '&Itemid='.$menu['id'];
-                } else {
-                    if($params_itemid!=''){
-                        $itemid = '&Itemid='.$params_itemid;
-                    }
-                }
-                if ($request_type == 'json'){
-                    $rows[$key]->text = $row->cat_agecat;
-                }
+				//get Itemid for each search-result. Seach first for lupo-category menuitem, if not set take plugin-Itemid if set
+				$query	= $db->getQuery(true);
+				$query->select('m.id');
+				$query->from('#__menu AS m');
+				$query->where('m.link = "index.php?option=com_lupo&view=category&id='.$row->catslug.'"');
+				$db->setQuery($query);
+				$menu = $db->loadAssoc();
+				$itemid = "";
+				if(isset($menu['id'])){
+					$itemid = '&Itemid='.$menu['id'];
+				} else {
+					if($params_itemid!=''){
+						$itemid = '&Itemid='.$params_itemid;
+					}
+				}
+				if ($request_type == 'json'){
+					$rows[$key]->text = $row->cat_agecat;
+				}
 				$rows[$key]->href = 'index.php?option=com_lupo&view=game&id='.$row->id.$itemid;
 
-
-				//check if image exists
-				$image = $model->getGameFoto($row->number, 'thumb_');
-				$rows[$key]->image = $image['image_thumb'];
+				$rows[ $key ]->image = false;
+				if($foto_show) {
+					//check if image exists
+					$image               = $model->getGameFoto( $row->number, 'thumb_' );
+					$rows[ $key ]->image = $image['image_thumb'];
+				}
 			}
 		}
-	
+
 		return $rows;
 	}
 }
