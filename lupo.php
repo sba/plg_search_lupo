168b20f80d6a22501cba83a4c83d23036c4fc25f
Stefan Bauer <sba@gmx.ch>
2021-11-09 11:28:40 +0100

    cs

--- lupo.php
+++ lupo.php
@@ -12,18 +12,18 @@ defined('_JEXEC') or die;
 
 jimport('joomla.plugin.plugin');
 
-class plgSearchLupo extends JPlugin
-{
+class plgSearchLupo extends JPlugin {
 	/**
 	 * Constructor
 	 *
 	 * @access      protected
-	 * @param       object  $subject The object to observe
-	 * @param       array   $config  An array that holds the plugin configuration
+	 *
+	 * @param   object  $subject  The object to observe
+	 * @param   array   $config   An array that holds the plugin configuration
+	 *
 	 * @since       1.5
 	 */
-	public function __construct(& $subject, $config)
-	{
+	public function __construct(&$subject, $config) {
 		parent::__construct($subject, $config);
 		$this->loadLanguage();
 	}
@@ -31,11 +31,11 @@ class plgSearchLupo extends JPlugin
 	/**
 	 * @return array An array of search areas
 	 */
-	function onContentSearchAreas()
-	{
+	function onContentSearchAreas() {
 		static $areas = array(
 			'lupo' => 'PLG_SEARCH_LUPO_TOYS'
 		);
+
 		return $areas;
 	}
 
@@ -44,21 +44,21 @@ class plgSearchLupo extends JPlugin
 	 *
 	 * The sql must return the following fields that are used in a common display
 	 * routine: href, title, section, created, text, browsernav
-	 * @param string Target search string
-	 * @param string mathcing option, exact|any|all
-	 * @param string ordering option, newest|oldest|popular|alpha|category
+	 *
+	 * @param   string Target search string
+	 * @param   string mathcing option, exact|any|all
+	 * @param   string ordering option, newest|oldest|popular|alpha|category
 	 */
-	function onContentSearch($text, $phrase='', $ordering='', $areas=null)
-	{
-		if (!class_exists( 'LupoModelLupo' )){
-			JLoader::import( 'lupo', JPATH_SITE . '/components/com_lupo/models' );
+	function onContentSearch($text, $phrase = '', $ordering = '', $areas = null) {
+		if (!class_exists('LupoModelLupo')) {
+			JLoader::import('lupo', JPATH_SITE . '/components/com_lupo/models');
 		}
 		$model = new LupoModelLupo();
 
-		$db		= JFactory::getDbo();
-		$app	= JFactory::getApplication();
-		$user	= JFactory::getUser();
-		$groups	= implode(',', $user->getAuthorisedViewLevels());
+		$db     = JFactory::getDbo();
+		$app    = JFactory::getApplication();
+		$user   = JFactory::getUser();
+		$groups = implode(',', $user->getAuthorisedViewLevels());
 
 		$request_type = JRequest::getVar('type');
 
@@ -68,9 +68,9 @@ class plgSearchLupo extends JPlugin
 			}
 		}
 
-		$limit			= $this->params->def('search_limit',50);
-		$foto_show      = $this->params->def('foto_show','1');
-		$params_itemid  = $this->params->def('Itemid','');
+		$limit         = $this->params->def('search_limit', 50);
+		$foto_show     = $this->params->def('foto_show', '1');
+		$params_itemid = $this->params->def('Itemid', '');
 
 		$text = trim($text);
 		if ($text == '') {
@@ -97,10 +97,9 @@ class plgSearchLupo extends JPlugin
 
 
 		//search for exact search-phrase
-		$words = array($text);
+		$words  = array($text);
 		$wheres = array();
-		foreach ($words as $word)
-		{
+		foreach ($words as $word) {
 			$word      = $db->quote('%' . $db->escape($word, true) . '%', false);
 			$wheres2   = array();
 			$wheres2[] = 'LOWER(a.title) LIKE LOWER(' . $word . ')';
@@ -112,54 +111,54 @@ class plgSearchLupo extends JPlugin
 		}
 		$where = '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
 
-		$query	= $db->getQuery(true);
+		$query = $db->getQuery(true);
 		$query->select('a.id
 						, a.title as title
 						, "" AS created
 						, a.id AS slug
 						, c.id AS catslug
 						, CONCAT (a.description_title, " ", a.description) as text
-						, CONCAT("'.$section.'", " / ", c.title, IF(a.age_catid<>"", CONCAT(", " , ac.title),"")) AS section
+						, CONCAT("' . $section . '", " / ", c.title, IF(a.age_catid<>"", CONCAT(", " , ac.title),"")) AS section
 						, CONCAT(c.title, " / ", ac.title) as cat_agecat
 						, "2" AS browsernav
 						, a.number');
 		$query->from('#__lupo_game AS a');
 		$query->leftJoin('#__lupo_categories AS c ON c.id = a.catid');
 		$query->leftJoin('#__lupo_agecategories AS ac ON ac.id = a.age_catid');
-		$query->where($where . ' OR a.number = '. $db->quote($db->escape($text, true)));
+		$query->where($where . ' OR a.number = ' . $db->quote($db->escape($text, true)));
 		$query->order($order);
 		$db->setQuery($query, 0, $limit);
 
 		$rows = $db->loadObjectList();
 
 		if ($rows) {
-			foreach($rows as $key => $row) {
+			foreach ($rows as $key => $row) {
 
 				//get Itemid for each search-result. Seach first for lupo-category menuitem, if not set take plugin-Itemid if set
-				$query	= $db->getQuery(true);
+				$query = $db->getQuery(true);
 				$query->select('m.id');
 				$query->from('#__menu AS m');
-				$query->where('m.link = "index.php?option=com_lupo&view=category&id='.$row->catslug.'"');
+				$query->where('m.link = "index.php?option=com_lupo&view=category&id=' . $row->catslug . '"');
 				$db->setQuery($query);
-				$menu = $db->loadAssoc();
+				$menu   = $db->loadAssoc();
 				$itemid = "";
-				if(isset($menu['id'])){
-					$itemid = '&Itemid='.$menu['id'];
+				if (isset($menu['id'])) {
+					$itemid = '&Itemid=' . $menu['id'];
 				} else {
-					if($params_itemid!=''){
-						$itemid = '&Itemid='.$params_itemid;
+					if ($params_itemid != '') {
+						$itemid = '&Itemid=' . $params_itemid;
 					}
 				}
-				if ($request_type == 'json'){
+				if ($request_type == 'json') {
 					$rows[$key]->text = $row->cat_agecat;
 				}
-				$rows[$key]->href = 'index.php?option=com_lupo&view=game&id='.$row->number.$itemid;
+				$rows[$key]->href = 'index.php?option=com_lupo&view=game&id=' . $row->number . $itemid;
 
-				$rows[ $key ]->image = false;
-				if($foto_show) {
+				$rows[$key]->image = false;
+				if ($foto_show) {
 					//check if image exists
-					$image               = $model->getGameFoto( $row->number, '' );
-					$rows[ $key ]->image = $image['image_thumb'];
+					$image             = $model->getGameFoto($row->number, '');
+					$rows[$key]->image = $image['image_thumb'];
 				}
 			}
 		}
